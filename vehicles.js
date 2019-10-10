c9ec66f148d0bcb84be3687f9e10bd1d4404f67b
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-10-10 10:54:56 -0400

    promise

---
+++ vehicles.js
@@ -0,0 +1,18 @@
+var setBanner = function(message)
+             {
+             d3.select(#banner)
+                .text("The Vehicles of Ghibli");
+             }
+             
+var vehiclesPromise = d3.json("vehicles.json")
+            vehiclesPromise.then(
+            function(vehicles)
+                {
+            setBanner("The Vehicles of Ghibli");
+            console.log("Vehicles", vehicles);
+            make VehicleBar(vehicles);
+                }
+            function(err)
+                {setBanner("No Transportation Today");
+                })
+            
