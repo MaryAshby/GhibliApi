4f8b9934269d70799a8b9af1a4708cd3df14dc58
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-10-14 19:30:35 -0400

    append

--- vehicles.js
+++ vehicles.js
@@ -3,19 +3,12 @@ var setBanner = function(message)
              d3.select(#banner)
                 .text("The Vehicles of Ghibli");
              }
-             
-var vehiclesPromise = d3.json("https://ghibliapi.herokuapp.com/vehicles")
-            vehiclesPromise.then(
-            function(vehicles)
+
+var displayInfo = function(vehicle)
                 {
-            setBanner("The Vehicles of Ghibli");
-            console.log("Vehicles", vehicles);
-            makeVehicleBar(vehicles);
+                  clearInfo()
                 }
-            function(err)
-                {setBanner("No Transportation Today");
-                })
- 
+
 var makeVehicleBar=function(vehicles)
               {
                 d3.select("#fan_art")
@@ -30,11 +23,27 @@ var makeVehicleBar=function(vehicles)
                         {
                          displayInfo(d);
                 })
-                        
-var displayInfo = function(vehicle)
+             
+var vehiclesPromise = d3.json("https://ghibliapi.herokuapp.com/vehicles")
+            vehiclesPromise.then(
+            function(vehicles)
                 {
-                  clearInfo()
+            setBanner("The Vehicles of Ghibli");
+            console.log("Vehicles", vehicles);
+            makeVehicleBar(vehicles);
                 }
+            function(err)
+                {setBanner("No Transportation Today");
+                })
+ 
+var info = box.append("div")
+              .attr("class","info);
+           info.append("div".text("name:" + name)
+           info.append("div".text("description:" + description)
+           info.append("div".text("vehicle class:" + vehicle_class)   
+           info.append("div".text("lenght:" + length)
+           info.append("div".text("pilot:" + pilot)
+           info.append("div".text("films:" + films)
                 
                 
                       
